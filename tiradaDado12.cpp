b5403a5db21a92fb51cdd86112b9997f2a62ce99
maxjgarcia <garcia.maximiliano.jesus@gmail.com>
2025-06-20 13:09:51 -0300

    fix de texto

--- tiradaDado12.cpp
+++ tiradaDado12.cpp
@@ -9,8 +9,8 @@ int tiradaDado12() {
     int dado12_1 = rand() % 12 + 1;
     int dado12_2 = rand() % 12 + 1;
 
-    cout << "Tirada del dado 1: " << dado12_1 <<endl;
-    cout << "Tirada del dado 2: " << dado12_2 <<endl;
+    cout << "Tirada del dado objectivo 1: " << dado12_1 <<endl;
+    cout << "Tirada del dado objectivo 2: " << dado12_2 <<endl;
 
     return dado12_1 + dado12_2;
 }
